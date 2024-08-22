52665ff958359fa36b67ad3e800f2111dda3c881
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-08-21 22:03:31 -0400

    adding id

--- src/main/java/kr/co/hanbit/product/management/domain/Product.java
+++ src/main/java/kr/co/hanbit/product/management/domain/Product.java
@@ -4,6 +4,10 @@ public class Product {
     private Long id;
     private String name;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
